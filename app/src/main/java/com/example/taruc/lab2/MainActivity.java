45e69a949d3589ea570098ddef3e8207bc9b39ce
wong chee foong <scollexwong@hotmail.com>
2018-10-30 11:00:09 +0800

    all done

--- app/src/main/java/com/example/taruc/lab2/MainActivity.java
+++ app/src/main/java/com/example/taruc/lab2/MainActivity.java
@@ -6,18 +6,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     public static final String EXTRA_MESSAGE = "com.example.taruc.lab2.extra.MESSAGE";
     private EditText mMessageEditText;
-
+    public static final int TEXT_REQUEST = 1;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mMessageEditText = findViewById(R.id.editText);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView=findViewById(R.id.text_mesage_reply);
     }
 
     public void launchSecondActivity(View view) {
@@ -25,6 +30,19 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this,activity_2.class);
         String message = mMessageEditText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE,message);
-        startActivity(intent);
+        startActivityForResult(intent,TEXT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == TEXT_REQUEST){
+            if(resultCode == RESULT_OK){
+                String reply = data.getStringExtra(activity_2.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/taruc/lab2/activity_2.java
+++ app/src/main/java/com/example/taruc/lab2/activity_2.java
@@ -7,18 +7,30 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class activity_2 extends AppCompatActivity {
 
+    public static final String EXTRA_REPLY = "om.example.taruc.lab2.extra.REPLY";
+    private EditText mReply;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        mReply = findViewById(R.id.editText_second);
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
+
     }
 
+    public void returnReply(View view) {
+        Intent replyIntent = new Intent();
+        String reply = mReply.getText().toString();
+        replyIntent.putExtra(EXTRA_REPLY,reply);
+        setResult(RESULT_OK,replyIntent);
+        finish();
+    }
 }
